fd063ad873574c1b1c6f92a6093177dea3f43281
Alessandro Simoni <alesimoni2000@gmail.com>
2023-04-19 17:10:28 +0200

    FINITOOOO

--- Lab2/NumeriPrimi.java
+++ Lab2/NumeriPrimi.java
@@ -1,24 +1,34 @@
+/*
+	Gruppo 18: Un viaggio
+	Componenti:
+		* Alessandro Simoni S5029301
+		* Simone Lutero S4801326
+		* Eleonora Fabbri S4842235
+		* Samuele Osti S4816869
+*/
+
 package Lab2;
 
 import java.util.*;
 
 public class NumeriPrimi {
     public static void main(String[] args) {
-        //Random rand = new Random();
-        int n = 10;
+        Random rand = new Random();
+        int n = rand.nextInt(100)+1;
 
 
         ArrayList<Integer> numberList = new ArrayList<>();
 
-        for(int i=0; i<n; i++){
-            numberList.add(i+2);
+        for(int i=1; i<n; i++){
+            numberList.add(i);
         }
 
         for(int i=0; i<numberList.size(); i++){
             System.out.print(numberList.get(i) + " ");
         }
+        System.out.println("");
 
         MyThread my_t = new MyThread(numberList, 1);
-        my_t.run();
+        my_t.start();
     }
 }
