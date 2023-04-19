4dd194dcd8fbf5a9daf6575b15866a16a9ba3a1c
Alessandro Simoni <alesimoni2000@gmail.com>
2023-04-19 15:02:08 +0200

    Update lab2.java

--- Lab2/lab2.java
+++ Lab2/lab2.java
@@ -7,17 +7,13 @@ import java.util.LinkedList;
 public class lab2 {
     public static void main(String[] args) {
         Random rand = new Random();
-        int prime = 0; 
         int n = 10;
 
 
         ArrayList<Integer> numberList = new ArrayList<>();
 
         for(int i=0; i<n; i++){
-            prime++;
-            if(isPrime(prime)){
-                numberList.set(i, prime);
-            }
+            numberList.set(i, i);
         }
 
         for(int i=0; i<numberList.size(); i++){
