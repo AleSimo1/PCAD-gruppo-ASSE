f8d4a7e69df299358d1e6b9fcdebd463208c5ee8
Alessandro Simoni <alesimoni2000@gmail.com>
2023-04-19 16:38:00 +0200

    Finish?

---
+++ Lab2/MyThread.java
@@ -0,0 +1,32 @@
+package Lab2;
+
+import java.util.*;
+
+public class MyThread extends Thread {
+    private Thread t = new Thread();
+    private int n_keep = 0;
+    private int n_prime;
+    private ArrayList<Integer> numberStart = new ArrayList<>();
+    private ArrayList<Integer> numberNext = new ArrayList<>();
+    private Thread t_next = new Thread();
+
+    public MyThread(ArrayList<Integer> numberStart, int n_prime) {
+        this.numberStart = numberStart;
+        this.n_prime = n_prime;
+    }
+
+    public void run() {
+        for(int i=0; i<numberStart.size(); i++){
+                if(numberStart.get(i) % numberStart.get(n_prime) != 0 || numberStart.get(n_prime) == numberStart.get(i)){
+                    numberNext.add(i);
+            }
+        }
+        MyThread t_next = new MyThread(numberNext, n_prime++);
+        if(numberNext.size() != n_prime)
+            t_next.run();
+        else
+        for(int i=0; i<numberNext.size(); i++){
+            System.out.print(numberNext.get(i) + " ");
+        }
+    }
+}
